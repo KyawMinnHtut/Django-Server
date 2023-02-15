7255804d7e71c00c84f1aa1772b5d2682376f0be
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-15 11:23:38 +0630

    Update models.py

--- channels/models.py
+++ channels/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Channels(models.Model):
     title = models.CharField(max_length=25)
     img = models.CharField(max_length=100)
-    link = models.CharField(max_length=100)
+    link = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title  
