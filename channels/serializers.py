72e674117cce535913c495dc833047ab27d8b0a4
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-15 17:19:57 +0630

    Update serializers.py

--- channels/serializers.py
+++ channels/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
-from .models import Channels
+from .models import Channels, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
 
 class ChannelSerializer(serializers.ModelSerializer):
     class Meta:
