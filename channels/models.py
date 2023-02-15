46550e7706f877aa52da2d9c4caaa8d54646409c
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-15 17:19:04 +0630

    Update models.py

--- channels/models.py
+++ channels/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length=30)
+    def __str__(self):
+        return self.category
+
 class Channels(models.Model):
     title = models.CharField(max_length=25)
     img = models.CharField(max_length=100)
     link = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title  
