9bcf84ecde98ae7c17d3a8d20ef43254ac531c4b
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-15 17:53:19 +0630

    Update admin.py

--- channels/admin.py
+++ channels/admin.py
@@ -10,5 +10,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Channels)
 class ChannelsAdmin(admin.ModelAdmin):
-    list_display = ("title", "img", "link")
+    list_display = ("title", "img", "link", "category")
     search_fields = ("title__startswith",)
