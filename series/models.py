6530b647028ef244cc59fe5f9fd80a21a717794d
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-15 11:25:35 +0630

    Update models.py

--- series/models.py
+++ series/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Link(models.Model):
     name = models.CharField(max_length=25)
-    link = models.CharField(max_length=30)
+    link = models.CharField(max_length=255)
     def __str__(self):
         return self.name
 
