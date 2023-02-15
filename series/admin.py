d59090eed40078a0f10f3d902afffb126d0ee0d9
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-15 17:10:07 +0630

    Update admin.py

--- series/admin.py
+++ series/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from .models import Series, Part, Link
+from .models import Series, Part, Link, Category
 
 # Register your models here.
 # admin.site.register(Category)
 # admin.site.register(Movie)
 
-# @admin.register(Category)
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ("category",)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("category",)
 
 @admin.register(Series)
 class SeriesAdmin(admin.ModelAdmin):
