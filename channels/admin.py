a3126e79e07c6c98d0f331aaace7da919897363e
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-15 17:24:50 +0630

    Update admin.py

--- channels/admin.py
+++ channels/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Channels
+from .models import Channels, Category
 
 # Register your models here.
 # admin.site.register(Channels)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("category",)
+
 @admin.register(Channels)
 class ChannelsAdmin(admin.ModelAdmin):
     list_display = ("title", "link")
