4dd104942432c81cadd03f43d3884efa7539dc68
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-15 17:03:55 +0630

    Update views.py

--- series/views.py
+++ series/views.py
@@ -1,9 +1,15 @@
 from rest_framework import viewsets
 from .models import Series
-from .serializers import SeriesSerializer
+from .serializers import SeriesSerializer, CategorySerializer
 
 
 # Create your views here.
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    '''API endpoint that allows users to be viewed.'''
+    queryset = Category.objects.all().order_by('-id')
+    serializer_class = CategorySerializer
+
 class SeriesViewSet(viewsets.ReadOnlyModelViewSet):
     '''API endpoint that allows users to be viewed.'''
     queryset = Series.objects.all().order_by('-id')
