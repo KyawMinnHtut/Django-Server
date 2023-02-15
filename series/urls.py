da1bf2e4b2fccd4cd2d1ae2c6b0eb77ecdc3b653
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-15 17:06:08 +0630

    Update urls.py

--- series/urls.py
+++ series/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
+router.register(r'category', views.CategoryViewSet)
 router.register(r'', views.SeriesViewSet)
 
 urlpatterns = [
