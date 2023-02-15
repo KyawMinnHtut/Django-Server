985a522ff5f77f1194865f561cd23aa14725bcad
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-15 17:21:22 +0630

    Update views.py

--- channels/views.py
+++ channels/views.py
@@ -1,9 +1,15 @@
 from rest_framework import viewsets
-from .models import Channels
-from .serializers import ChannelSerializer
+from .models import Channels, Category
+from .serializers import ChannelSerializer, CategorySerializer
 
 
 # Create your views here.
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    '''API endpoint that allows users to be viewed.'''
+    queryset = Category.objects.all().order_by('-id')
+    serializer_class = CategorySerializer
+
 class ChannelViewSet(viewsets.ReadOnlyModelViewSet):
     '''API endpoint that allows users to be viewed.'''
     queryset = Channels.objects.all().order_by('title')
