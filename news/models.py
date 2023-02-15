b2400b3788d6116279dbc0cee5b87c32e933db86
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-15 23:53:23 +0630

    Update models.py

--- news/models.py
+++ news/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class News(models.Model):
     # premium = models.BooleanField(default=False)
     date = models.DateField(auto_now=False, auto_now_add=False)
-    title = models.CharField(max_length=50, blank=True)
-    img = models.CharField(max_length=100, blank=True)
+    title = models.CharField(max_length=255, blank=True)
+    img = models.CharField(max_length=255, blank=True)
     body = models.TextField(blank=True, null=True)
     def __str__(self):
         return self.title
