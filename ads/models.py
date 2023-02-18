5eb1a1211bde64a321bba04ce77420571b9f495a
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-18 22:14:56 +0630

    Update models.py

--- ads/models.py
+++ ads/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+SCREENS = (
+    ("live", "live"),
+    ("highlights", "highlights"),
+    ("channels", "channels"),
+)
+
 class Ads(models.Model):
     img = models.CharField(max_length=255)
     link = models.CharField(max_length=255)
@@ -13,9 +20,10 @@ class ImageAds(models.Model):
     img = models.CharField(max_length=255)
     link = models.CharField(max_length=255)
     messenger = models.CharField(max_length=10)
+    screen = models.CharField(max_length=15, choices = SCREENS, default="live")
 
     def __str__(self):
-        return self.link 
+        return self.link + " ("+self.screen+")" 
 
 class TextAds(models.Model):
     text = models.CharField(max_length=255)
