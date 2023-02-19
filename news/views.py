7634224503038b7dc108856c0f20ffe8500fd4fa
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-19 17:19:13 +0630

    Update views.py

--- news/views.py
+++ news/views.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets
 from .models import News
 from .serializers import NewsSerializer
-
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class NewsViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = (IsAuthenticated,)
     '''API endpoint that allows users to be viewed.'''
     queryset = News.objects.all().order_by('-id')
     serializer_class = NewsSerializer
