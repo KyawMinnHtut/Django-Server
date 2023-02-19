0514d760291292af31f3eb579fd709dd262604ba
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-19 15:44:49 +0630

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,7 +1,16 @@
 from rest_framework import viewsets
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super(CustomObtainAuthToken, self).post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({'token': token.key, 'id': token.user_id})
 
 # Create your views here.
 class CustomUserViewSet(viewsets.ModelViewSet):
