2366689fdd14d5a0d81f8c04875e0ce11d512d8b
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-19 17:08:44 +0630

    Update views.py

--- highlights/views.py
+++ highlights/views.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets
 from .models import HighLight
 from .serializers import LiveSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
 class HighLightViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = (IsAuthenticated,)
     '''API endpoint that allows users to be viewed.'''
     queryset = HighLight.objects.all().order_by('-id')
     serializer_class = LiveSerializer
