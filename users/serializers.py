0192980b22dcecaf41aed5d1c98a197f9ccaecdf
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-19 17:31:42 +0630

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ("id", "username", "premium_date")
+        fields = ("id", "username", "password", "premium_date")
