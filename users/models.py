cd11737c68231a62aeb038cdc53d94a11c142f9c
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-19 18:31:01 +0630

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.timezone import now
+from .manager import CustomUserManager
 
 # Create your models here.
 class CustomUser(AbstractUser):
     premium_date = models.DateField(default=now, blank=True)
+    
+    objects = CustomUserManager()
+    
+    def __str__(self):
+        return self.username
