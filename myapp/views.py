dc505a223519ef5382ffc47d479a2a219d9af771
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-19 17:18:35 +0630

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets
 from .models import Live
 from .serializers import LiveSerializer
-
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class LiveViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = (IsAuthenticated,)
     '''API endpoint that allows users to be viewed.'''
     queryset = Live.objects.all().order_by('-id')
     serializer_class = LiveSerializer
