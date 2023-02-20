1ac50a1cbc8efeece11bc84ab7571453d61ce0ff
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-20 12:18:18 +0630

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
-from rest_framework import viewsets
+from django.http import Http404
 from .models import CustomUser
-from .serializers import CustomUserSerializer
+#from .serializers import CustomUserSerializer, CreateUserSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework import status
 from rest_framework.response import Response
-
+from django.contrib.auth.hashers import make_password
 
 class CustomAuthToken(ObtainAuthToken):
 
@@ -20,7 +22,36 @@ class CustomAuthToken(ObtainAuthToken):
         })
 
 # Create your views here.
-class CustomUserViewSet(viewsets.ModelViewSet):
-    '''API endpoint that allows users to be viewed.'''
-    queryset = CustomUser.objects.all().order_by('-id')
-    serializer_class = CustomUserSerializer
+#class CustomUserViewSet(viewsets.ModelViewSet):
+#    '''API endpoint that allows users to be viewed.'''
+#    queryset = CustomUser.objects.all().order_by('-id')
+#    serializer_class = CustomUserSerializer
+
+class CreateUser(APIView):
+    #permission_classes = [AllowAny]
+
+    def post(self, request, format='json'):
+        print(request.data)
+        data = request.data
+        reg_serializer = CreateUserSerializer(data=data)
+        if reg_serializer.is_valid():
+            password = reg_serializer.validated_data.get('password')
+            reg_serializer.validated_data['password']=make_password(password)
+            new_user = reg_serializer.save()
+            if new_user:
+                return Response(status=status.HTTP_201_CREATED)
+        return Response(reg_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+class UserDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return CustomUser.objects.get(pk=pk)
+        except CustomUser.DoesNotExist:
+            raise Http404
+
+
+    def get(self, request, pk, format='json'):
+        user = self.get_object(pk)
+        serializer = CustomUserDetailsSerializer(user)
+        return Response(serializer.data)
