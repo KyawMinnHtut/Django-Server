8c52d27e415347189f649558cd0b99271a9a99e0
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-20 12:12:33 +0630

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,20 @@
 from .models import CustomUser
 from rest_framework import serializers
 
-class CustomUserSerializer(serializers.ModelSerializer):
+#class CustomUserSerializer(serializers.ModelSerializer):
+#    class Meta:
+#        model = CustomUser
+#        fields = ("id", "username", "password", "premium_date")
+
+class CreateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ("id", "username", "password", "premium_date")
+        fields = ("username", 'password')
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
+
+class CustomUserDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ("id", "username", "premium_date")
