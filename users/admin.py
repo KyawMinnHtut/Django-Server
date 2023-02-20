d73a609c4da02d3141a9385c5647708b87c24720
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-20 12:20:48 +0630

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from django.contrib.auth.models import Group
+
+admin.site.unregister(Group)
 
 # Register your models here.
 @admin.register(CustomUser)
