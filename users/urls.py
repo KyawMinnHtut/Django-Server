811034431df1687e01b8679b8e2978606da263ba
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-02-20 12:20:01 +0630

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
+#from rest_framework import routers
 from . import views
 #from django.conf.urls import url
 
-router = routers.DefaultRouter()
-router.register(r'', views.CustomUserViewSet)
+#router = routers.DefaultRouter()
+#router.register(r'', views.CustomUserViewSet)
 
 urlpatterns = [
 #    path(r'^authenticate/', CustomObtainAuthToken.as_view()),
-    path('', include(router.urls))
+    path('user/', views.CreateUser.as_view()),
+    path('user/<int:pk>/', views.UserDetail.as_view()),
 ]
