c90aff33143d16bce26344d8bdcb731afffa8330
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-05-20 01:37:28 +0630

    Update urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from . import views
+from .views import FilteredMovieView
 
 router = routers.DefaultRouter()
 router.register(r'category', views.CategoryViewSet)
@@ -8,4 +9,5 @@ router.register(r'', views.MovieViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('/<int:category_id>/', FilteredMovieView.as_view())
 ]
