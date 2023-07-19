5c082bd30a21c235bef0e81cc1e1ee245e17907b
KyawMinnHtut <68686327+KyawMinnHtut@users.noreply.github.com>
2023-07-19 10:07:44 +0630

    Update views.py

--- movies/views.py
+++ movies/views.py
@@ -9,12 +9,12 @@ from rest_framework.permissions import IsAuthenticated
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = (IsAuthenticated,)
     '''API endpoint that allows users to be viewed.'''
-    queryset = Category.objects.all().order_by('-id')[:50]
+    queryset = Category.objects.all().order_by('-id')
     serializer_class = CategorySerializer
 
 class MovieViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = (IsAuthenticated,)
-    queryset = Movie.objects.all().order_by('-id')
+    queryset = Movie.objects.all().order_by('-id')[:50]
     serializer_class = MovieSerializer
 
 class FilteredMovieView(APIView):
